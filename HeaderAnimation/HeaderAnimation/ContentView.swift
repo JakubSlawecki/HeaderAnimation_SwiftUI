c1fe7fede030eb955f96eb55a6bc43f137258428
Jakub Slawecki <jakub.slawecki@hemmersbach.com>
2020-08-30 20:28:31 +0200

    initial commit

---
+++ HeaderAnimation/HeaderAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HeaderAnimation
+//
+//  Created by Jakub Slawecki on 30/08/2020.
+//  Copyright © 2020 Jakub Slawecki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
